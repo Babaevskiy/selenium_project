1e444e42bd0b466e6eb24c8e52b3e2557c97243e
Nick Babaevskiy <nikolaibabaevskiy@gmail.com>
2020-12-11 17:18:17 +0300

    add fixture

--- test_yandex_search.py
+++ test_yandex_search.py
@@ -2,29 +2,37 @@ from selenium import webdriver
 import allure
 
 
-@allure.title('Результат поиска больше 10')
-@allure.severity(severity_level='BLOCKER')
-def test_yandex_search():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
-    with allure.step('Открываем страницу поиска'):
-        driver.get('https://ya.ru')
+class TestYandex:
 
-    with allure.step('Ищем market.yandex.ru'):
-        search_input = driver.find_element_by_css_selector('[name="text"]')
-        search_button = driver.find_element_by_css_selector('[type="submit"]')
-        search_input.send_keys('market.yandex.ru')
-        search_button.click()
-        search_results = driver.find_elements_by_css_selector('[class="serp-item"]')
+    # def setup_method(self):
+    #     self.driver = webdriver.Chrome()
+    #     self.driver.implicitly_wait(5)
+    #
+    # def teardown_method(self):
+    #     self.driver.quit()
 
-    with allure.step('Ожидаем что количество результатов теста будет больше 10'):
-        assert len(search_results) >= 10, 'Количество результатов поиска меньше 10'
+    @allure.title('Результат поиска больше 10')
+    @allure.severity(severity_level='BLOCKER')
+    def test_yandex_search(self, driver):
 
-    with allure.step('Переходим по ссылке второго результата'):
-        link = search_results[1].find_element_by_css_selector('[class="organic__url-text"]')
-        link.click()
+        with allure.step('Открываем страницу поиска'):
+            driver.get('https://ya.ru')
 
-    driver.switch_to.window(driver.window_handles[1])
-    with allure.step('Проверяем корректность title страницы'):
-        assert driver.title == 'Яндекс.Маркет — выбор и покупка товаров из проверенных интернет-магазинов'
+        with allure.step('Ищем market.yandex.ru'):
+            search_input = driver.find_element_by_css_selector('[name="text"]')
+            search_button = driver.find_element_by_css_selector('[type="submit"]')
+            search_input.send_keys('market.yandex.ru')
+            search_button.click()
+            search_results = driver.find_elements_by_css_selector('[class="serp-item"]')
+
+        with allure.step('Ожидаем что количество результатов теста будет больше 10'):
+            assert len(search_results) >= 10, 'Количество результатов поиска меньше 10'
+
+        with allure.step('Переходим по ссылке второго результата'):
+            link = search_results[1].find_element_by_css_selector('[class="organic__url-text"]')
+            link.click()
+
+        driver.switch_to.window(driver.window_handles[1])
+        with allure.step('Проверяем корректность title страницы'):
+            assert driver.title == 'Яндекс.Маркет — выбор и покупка товаров из проверенных интернет-магазинов'
 
